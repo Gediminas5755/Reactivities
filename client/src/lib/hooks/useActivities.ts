c61fd787cdfc6f530ca4064b04a2f0fa1808f208
Gediminas5755 <gediminas.sumskas@gmail.com>
2026-01-29 18:31:02 +0200

    s13e135

--- client/src/lib/hooks/useActivities.ts
+++ client/src/lib/hooks/useActivities.ts
@@ -1,18 +1,21 @@
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import agent from "../api/agent";
 import { useLocation } from "react-router";
+import { useAccount } from "./useAccount";
 
 export const useActivities = (id? : string) => {
     const queryClient = useQueryClient();
+    const { currentUser } = useAccount();
     const location = useLocation();
 
-    const { data: activities, isPending } = useQuery({
+    const { data: activities, isLoading } = useQuery({
         queryKey: ['activities'],
         queryFn: async () => {
             const response = await agent.get<Activity[]>("/activities");
             return response.data;
         },
-        enabled: !id || location.pathname === '/activities', //disable when id is present and not in activities list page
+        enabled: !id && location.pathname === '/activities' && !!currentUser //disable when id is present and not in activities list page
+        
         // staleTime: 5 * 60 * 1000, //5 minutes
     });
 
@@ -22,7 +25,7 @@ export const useActivities = (id? : string) => {
             const response = await agent.get<Activity>(`/activities/${id}`);
             return response.data;
         },
-        enabled: !!id//id not null or undefined, both useQuery would be executed
+        enabled: !!id && !!currentUser //id not null or undefined, both useQuery would be executed
     });
 
     const updateActivity = useMutation({
@@ -55,7 +58,7 @@ export const useActivities = (id? : string) => {
 
     return { 
         activities, 
-        isPending, 
+        isLoading, 
         updateActivity, 
         createActivity, 
         deleteActivity, 
