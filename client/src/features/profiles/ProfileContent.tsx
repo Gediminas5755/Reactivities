3d5207a86a7fb64df354c607395ddecce3a6a30d
Gediminas5755 <gediminas.sumskas@gmail.com>
2026-04-02 14:23:43 +0300

    s17e177 profile content initial

--- client/src/features/profiles/ProfileContent.tsx
+++ client/src/features/profiles/ProfileContent.tsx
@@ -1,5 +1,41 @@
+import { Box, Paper, Tab, Tabs } from "@mui/material";
+import React from "react";
+
 export default function ProfileContent() {
-  return (
-    <div>ProfileContent</div>
-  )
+    const [value, setValue] = React.useState(0);
+
+    const handleChange = (_: React.SyntheticEvent, newValue: number) => {
+        setValue(newValue);
+    }
+
+    const tabContent = [
+        { label: 'About', content: <div>About</div> },
+        { label: 'Photos', content: <div>Photos</div> },
+        { label: 'Events', content: <div>Events</div> },
+        { label: 'Followers', content: <div>Followers</div> },
+        { label: 'Following', content: <div>Following</div> },
+    ];
+
+    return (
+        <Box
+            component={Paper}
+            mt={2}
+            elevation={3}
+            height={500}
+            sx={{ display: 'flex', alignItems: 'flex-start', borderRadius: 3 }}
+        >
+            <Tabs
+                orientation="vertical"
+                value={value}
+                onChange={handleChange}
+                sx={{ borderRight: 1, height: 450, minWidth: 150 }}>
+                {tabContent.map((tab, index) => (
+                    <Tab key={index} label={tab.label} sx={{ mr: 3 }} />
+                ))}
+            </Tabs>
+            <Box  p={3} flexGrow={1}>
+                {tabContent[value].content}
+            </Box>
+        </Box>
+    )
 }
