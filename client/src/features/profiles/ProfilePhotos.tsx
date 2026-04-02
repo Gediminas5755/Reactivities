566d4347e1641e5ed4eba521007af2d3d9c42030
Gediminas5755 <gediminas.sumskas@gmail.com>
2026-04-02 16:18:51 +0300

    s17e180 edit photos for current user

--- client/src/features/profiles/ProfilePhotos.tsx
+++ client/src/features/profiles/ProfilePhotos.tsx
@@ -3,26 +3,45 @@ import { useProfile } from "../../lib/hooks/useProfile";
 import Typography from "@mui/material/Typography/Typography";
 import ImageList from "@mui/material/ImageList/ImageList";
 import ImageListItem from "@mui/material/ImageListItem/ImageListItem";
+import React from "react";
+import Box from "@mui/material/Box/Box";
+import Button from "@mui/material/Button/Button";
 
 export default function ProfilePhotos() {
     const { id } = useParams();
-    const { photos, loadingPhotos } = useProfile(id);
+    const { photos, loadingPhotos, isCurrentUser } = useProfile(id);
+    const [editMode, setEditMode] = React.useState(false);
 
     if (loadingPhotos) return <Typography>Loading photos...</Typography>
     if (!photos || photos.length === 0) return <Typography>No photos found</Typography>;
 
     return (
-        <ImageList sx={{ height: 450 }} cols={6} rowHeight={164}>
-            {photos.map((item) => (
-                <ImageListItem key={item.id}>
-                    <img
-                        srcSet={`${item.url.replace('/upload', '/upload/w_164,h_164,c_fill,dpr_2,f_auto')}`}
-                        src={`${item.url.replace('/upload', '/upload/w_164,h_164,c_fill,dpr_2,f_auto')}`}
-                        alt={'Photo profile img'}
-                        loading="lazy"
-                    />
-                </ImageListItem>
-            ))}
-        </ImageList>
+        <Box>
+            {isCurrentUser && (
+                <Box>
+                    <Button onClick={() => setEditMode(!editMode)}>
+                        {editMode ? 'Cancel' : 'Manage Photos'}
+                    </Button>
+                </Box>
+            )}
+            {editMode ? (
+                <div>photo widget</div>
+            ) : (
+                <ImageList sx={{ height: 450 }} cols={6} rowHeight={164}>
+                    {photos.map((item) => (
+                        <ImageListItem key={item.id}>
+                            <img
+                                srcSet={`${item.url.replace('/upload', '/upload/w_164,h_164,c_fill,dpr_2,f_auto')}`}
+                                src={`${item.url.replace('/upload', '/upload/w_164,h_164,c_fill,dpr_2,f_auto')}`}
+                                alt={'Photo profile img'}
+                                loading="lazy"
+                            />
+                        </ImageListItem>
+                    ))}
+                </ImageList>
+            )}
+        </Box>
+
+
     )
 }
--- client/src/lib/hooks/useProfile.ts
+++ client/src/lib/hooks/useProfile.ts
@@ -1,23 +1,32 @@
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
 import agent from "../api/agent";
+import { useMemo } from "react";
 
 export const useProfile = (id?: string) => {
-    const {data : profile, isLoading : loadingProfile} = useQuery<Profile>({
+    const queryClint = useQueryClient();
+
+    const { data: profile, isLoading: loadingProfile } = useQuery<Profile>({
         queryKey: ['profile', id],
         queryFn: async () => {
             const response = await agent.get(`/profiles/${id}`);
             return response.data;
-        }
+        },
+        enabled: !!id, //only execute when id is not null or undefined
     })
 
 
-    const {data: photos, isLoading: loadingPhotos} = useQuery<Photo[]>({
-        queryKey: ['photos',id ],
+    const { data: photos, isLoading: loadingPhotos } = useQuery<Photo[]>({
+        queryKey: ['photos', id],
         queryFn: async () => {
             const response = await agent.get<Photo[]>(`/profiles/${id}/photos`);
             return response.data;
-        }
+        },
+        enabled: !!id, //only execute when id is not null or undefined
     })
 
-    return { profile, loadingProfile, photos, loadingPhotos }
+    const isCurrentUser = useMemo(() => {
+        return id === queryClint.getQueryData<User>(['user'])?.id;
+    }, [id, queryClint]);
+
+    return { profile, loadingProfile, photos, loadingPhotos, isCurrentUser };
 }
