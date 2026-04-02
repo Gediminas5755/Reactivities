1909e6312ada3d5a2d7be3e4ef6a37f368b3f6e1
Gediminas5755 <gediminas.sumskas@gmail.com>
2026-04-02 18:25:56 +0300

    s17e184 upload and set main photo

--- client/src/app/shared/components/PhotoUploadWidget.tsx
+++ client/src/app/shared/components/PhotoUploadWidget.tsx
@@ -1,12 +1,16 @@
 import { CloudUpload } from "@mui/icons-material";
-import { Box, Grid2, Typography } from "@mui/material";
+import { Box, Button, Grid2, Typography } from "@mui/material";
 import { useCallback, useRef, useState } from "react";
 import { useDropzone } from 'react-dropzone'
 import Cropper, { ReactCropperElement } from "react-cropper";
 import "cropperjs/dist/cropper.css";
-import { set } from "zod";
 
-export default function PhotoUploadWidget() {
+type Props = {
+    uploadPhoto: (file: Blob) => void
+    loading: boolean
+}
+
+export default function PhotoUploadWidget({ uploadPhoto, loading }: Props) {
     const [files, setFiles] = useState<object & { preview: string }[]>([]);
     const cropperRef = useRef<ReactCropperElement>(null);
     // const onCrop = () => {
@@ -22,6 +26,12 @@ export default function PhotoUploadWidget() {
         //console.log(acceptedFiles);
         // Do something with the files
     }, [])
+
+    const onCrop = useCallback(() => {
+        const cropper = cropperRef.current?.cropper;
+        cropper?.getCroppedCanvas().toBlob(blob => uploadPhoto(blob as Blob));
+    }, [uploadPhoto])
+
     const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop })
 
     return (
@@ -46,7 +56,7 @@ export default function PhotoUploadWidget() {
                 <Typography variant="overline" color="secondary">step 2: resize Photo</Typography>
                 {files[0]?.preview && (
                     <Cropper
-                        // ref={cropperRef}
+                        ref={cropperRef}
                         src={files[0]?.preview}
                         style={{ height: 280, width: '100%' }}
                         // Cropper options
@@ -66,6 +76,16 @@ export default function PhotoUploadWidget() {
                         <div
                             className="img-preview"
                             style={{ width: 300, height: 300, overflow: 'hidden' }} />
+                        <Button
+                            sx={{ mt: 2 }}
+                            onClick={onCrop}
+                            variant='contained'
+                            color="secondary"
+                            disabled={loading}
+                        >
+                            Upload
+                            { /* {loading ? 'Uploading...' : 'Upload Photo'} */}
+                        </Button>
                     </>
                 )}
             </Grid2>
--- client/src/lib/hooks/useProfile.ts
+++ client/src/lib/hooks/useProfile.ts
@@ -1,6 +1,7 @@
-import { useQuery, useQueryClient } from "@tanstack/react-query";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import agent from "../api/agent";
 import { useMemo } from "react";
+import { data } from "react-router";
 
 export const useProfile = (id?: string) => {
     const queryClint = useQueryClient();
@@ -24,9 +25,37 @@ export const useProfile = (id?: string) => {
         enabled: !!id, //only execute when id is not null or undefined
     })
 
+    const uploadPhoto = useMutation({
+        mutationFn: async (file: Blob) => {
+            const formData = new FormData();
+            formData.append('file', file);
+            const response = await agent.post('/profiles/add-photo', formData,  {
+                headers: { 'Content-Type': 'multipart/form-data' }
+            });
+            return response.data;
+        },
+        onSuccess: async (photo: Photo) => {
+            await queryClint.invalidateQueries({ queryKey: ['photos', id] }); //refresh photos after successful upload
+            queryClint.setQueryData(['user'], (data: User) => {
+                if (!data) return data;
+                return {
+                    ...data,
+                    imageUrl: data.imageUrl ?? photo.url //update user's main photo url after successful upload};
+                }
+            });
+            queryClint.setQueryData(['profile', id], (data: Profile) => {
+                if (!data) return data;
+                return {
+                    ...data,
+                    imageUrl: data.imageUrl ?? photo.url //update user's main photo url after successful upload};
+                }
+            });
+        }
+    })
+
     const isCurrentUser = useMemo(() => {
         return id === queryClint.getQueryData<User>(['user'])?.id;
     }, [id, queryClint]);
 
-    return { profile, loadingProfile, photos, loadingPhotos, isCurrentUser };
+    return { profile, loadingProfile, photos, loadingPhotos, isCurrentUser, uploadPhoto };
 }
