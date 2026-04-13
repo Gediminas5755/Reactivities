d9a8eed405456ec7e004353b859b267d0e81597e
Gediminas5755 <gediminas.sumskas@gmail.com>
2026-04-13 09:09:59 +0300

    s21e212 paged list activities initial

--- Application/Activities/Queries/GetActivityList.cs
+++ Application/Activities/Queries/GetActivityList.cs
@@ -1,5 +1,3 @@
-using System;
-using Domain;
 using MediatR;
 using Persistence;
 using Microsoft.EntityFrameworkCore;
@@ -8,19 +6,60 @@ using AutoMapper;
 using Application.Activities.DTOs;
 using AutoMapper.QueryableExtensions;
 using Application.Interfaces;
+using Application.Core;
 
 namespace Application.Activities.Queries;
 
 public class GetActivityList
 {
-    public class Querry : IRequest<List<ActivityDto>>
+    private const int MaxPageSize = 50;
+    public class Querry : IRequest<Result<PagedList<ActivityDto, DateTime?>>>
     {
+        public DateTime? Cursor { get; set; }
+        private int _pageSize = 3;
+        public int PageSize
+        {
+            get { return _pageSize; }
+            set { _pageSize = value > MaxPageSize ? MaxPageSize : value; }
+        }
+
     }
 
-    public class Handler(AppDbContext context, ILogger<GetActivityList> logger, IMapper mapper, IUserAccessor userAccessor) : IRequestHandler<Querry, List<ActivityDto>>
+    public class Handler(AppDbContext context, ILogger<GetActivityList> logger, IMapper mapper, IUserAccessor userAccessor)
+        : IRequestHandler<Querry, Result<PagedList<ActivityDto, DateTime?>>>
     {
-        public async Task<List<ActivityDto>> Handle(Querry request, CancellationToken cancellationToken)
+        public async Task<Result<PagedList<ActivityDto, DateTime?>>> Handle(Querry request, CancellationToken cancellationToken)
         {
+
+            var query = context.Activities
+                .OrderBy(a => a.Date)
+                .Where(a => a.Date >= request.Cursor)
+                .AsQueryable();
+
+            if (request.Cursor.HasValue)
+            {
+                query = query.Where(a => a.Date > request.Cursor.Value);
+            }
+
+            var activities = await query
+                .Take(request.PageSize + 1)
+                .ProjectTo<ActivityDto>(mapper.ConfigurationProvider, new { currentUserId = userAccessor.GetUserId() })
+                .ToListAsync(cancellationToken);
+
+            DateTime? nextCursor = null;
+            if (activities.Count > request.PageSize)
+            {
+                nextCursor = activities.Last().Date;
+                activities.RemoveAt(activities.Count - 1);
+            }
+
+            return Result<PagedList<ActivityDto, DateTime?>>.Success(
+                new PagedList<ActivityDto, DateTime?>
+                {
+                    Items = activities,
+                    NextCursor = nextCursor
+                });
+
             // try
             // {
             //     for (int i = 0; i < 10; i++)
@@ -34,9 +73,9 @@ public class GetActivityList
             // {
             //     logger.LogInformation(ex, "Task was cancelled");
             // }
-            return await context.Activities
-                .ProjectTo<ActivityDto>(mapper.ConfigurationProvider, new { currentUserId = userAccessor.GetUserId() })
-                .ToListAsync(cancellationToken);
+            // return await context.Activities
+            //     .ProjectTo<ActivityDto>(mapper.ConfigurationProvider, new { currentUserId = userAccessor.GetUserId() })
+            //     .ToListAsync(cancellationToken);
         }
     }
 }
